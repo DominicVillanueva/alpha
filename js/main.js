5917f0e1ea480f66b202332c7a6108f42cb0768e
Dominic <partner.dominic@gmail.com>
2020-05-07 19:19:54 -0500

    Cambios subidos ejemplo

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* | ===================================================|
+   | RyW NAGO | Importadora de Componentes Eléctricos.  |
+   | Version: 1.0                                       |
+   | ===================================================|
+*/
+
+'use strict';
+
+$(window).on('load', function() {
+    /* PRELOADER */
+    $(".loader").fadeOut();
+    $("#preloader").delay(400).fadeOut("slow");
+});
